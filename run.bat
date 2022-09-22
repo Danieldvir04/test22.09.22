51206af10d832a09e2a85ebfd99ac968d2093aee
Danieldvir04 <111674069+Danieldvir04@users.noreply.github.com>
2022-09-22 13:09:17 +0300

    Add files via upload
    daniel dvir test220922

---
+++ run.bat
@@ -0,0 +1,21 @@
+cd\
+>md TEST123
+>cd TEST123
+TEST123>md AAA
+TEST123>md BBB
+TEST123>md CCC
+TEST123>cd BBB
+TEST123\BBB>md DDD
+TEST123\BBB>cd..
+TEST123>cd ccc
+TEST123\CCC>dir list.txt
+TEST123\CCC>dir c:\Windows >list.txt
+TEST123\CCC>cd\
+>del/S/Q/test123
+rmdir/S/Q test123
+
+
+
+
+
+
